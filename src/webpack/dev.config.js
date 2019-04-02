02546e52d96c6167816be3e28009ab2be2c5e6c6
Sam Davidoff <samjdavidoff@gmail.com>
2019-04-02 13:30:53 -0400

    added css autoprefixer

--- src/webpack/dev.config.js
+++ src/webpack/dev.config.js
@@ -3,60 +3,68 @@ const merge = require('webpack-merge');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 const baseConfig = require('./base.config.js');
 
 module.exports = merge(baseConfig, {
-  devtool: 'eval-source-map',
+	devtool: 'eval-source-map',
 
-  module: {
-
-    rules: [
+	module: {
+		rules: [
 			{
 				test: /\.(scss|css)$/,
 				use: ExtractTextPlugin.extract({
 					fallback: 'style-loader',
 					use: [
 						{ loader: 'css-loader', options: { sourceMap: true } },
+						{
+							loader: 'postcss-loader',
+							options: {
+								plugins: () =>
+									autoprefixer({
+										browsers: ['last 3 versions', '> 1%']
+									})
+							}
+						},
 						{ loader: 'sass-loader', options: { sourceMap: true } }
 					],
 					fallback: 'style-loader'
 				})
 			}
-    ]
-
-  },
+		]
+	},
 
 	plugins: [
 		new ExtractTextPlugin('app.bundle.css'),
 
 		new BrowserSyncPlugin(
-		{
-			open: false,
-			logFileChanges: false,
-			port: 3000,
-			ui: { port: 3100 },
-			proxy: 'localhost:80',
-			files: [
-				{
-					match: [
-						path.join(__dirname, '../../assets/main.bundle.css'),
-						path.join(__dirname, '../../assets/main.bundle.js'),
-						path.join(__dirname, '../../**/*.php'),
-						path.join(__dirname, '../../**/*.html')
-					],
-					fn: function(event, file) {
-						if (event === "change") {
-							const bs = require('browser-sync').get('bs-webpack-plugin');
-							bs.reload();
+			{
+				open: false,
+				logFileChanges: false,
+				port: 3000,
+				ui: { port: 3100 },
+				proxy: 'localhost:80',
+				files: [
+					{
+						match: [
+							path.join(__dirname, '../../assets/main.bundle.css'),
+							path.join(__dirname, '../../assets/main.bundle.js'),
+							path.join(__dirname, '../../**/*.php'),
+							path.join(__dirname, '../../**/*.html')
+						],
+						fn: function(event, file) {
+							if (event === 'change') {
+								const bs = require('browser-sync').get('bs-webpack-plugin');
+								bs.reload();
+							}
 						}
 					}
-				}
-			]
-		},
-		{
-			reload: true
-		})
+				]
+			},
+			{
+				reload: true
+			}
+		)
 	]
-
 });
